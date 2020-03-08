afb43567fd68586f7449b501ecda1f1ccfa37c15
Ljube Boskovski <ljube.boskovski@tum.de>
2020-03-08 22:48:36 +0100

    Added global from stash

--- app/src/main/java/com/ljubeboskovski/drmario/Global.java
+++ app/src/main/java/com/ljubeboskovski/drmario/Global.java
@@ -6,14 +6,15 @@ public class Global {
 
     public static final int FRAMES_PER_SECOND = 60;
 
-    public static final int COORDS_PER_VERTEX = 3;
-    public static final int COLOR_LENGTH = 4;
-    public static final int STRIDE = (COORDS_PER_VERTEX + COLOR_LENGTH) * BYTES_PER_FLOAT;
+    public static final int SIZE_POSITION = 3;
+    public static final int SIZE_COLOR = 4;
+    public static final int SIZE_TEXTURE_COORDS = 2;
+    public static final int STRIDE = (SIZE_POSITION + SIZE_COLOR + SIZE_TEXTURE_COORDS) * BYTES_PER_FLOAT;
 
     public static enum BlockColor {
         RED,
         YELLOW,
         BLUE,
         GREEN
-    };
+    }
 }
